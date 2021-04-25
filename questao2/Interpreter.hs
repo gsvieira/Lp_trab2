c252bb45ae627dc8bee39f0f9bc7040f3a6df69c
luccahuguet <luccahuguet@hotmail.com>
2021-04-24 22:48:34 -0300

    Questão 2

--- questao2/Interpreter.hs
+++ questao2/Interpreter.hs
@@ -13,13 +13,13 @@ execute context x = case x of
    SBlock [] -> context
    SBlock (s:stms) -> execute (execute context s) (SBlock stms) 
    SWhile exp stm -> if ( i(eval context exp) /= 0) 
-                      then execute (execute context stm) (SWhile exp stm)
-                      else context
+                     then execute (execute context stm) (SWhile exp stm)
+                     else context
 
 
-data Valor = ValorStr String |
-             ValorInt Integer |
-             ValorBool Bool
+data Valor   = ValorStr String |
+               ValorInt Integer |
+               ValorBool Bool
 -- note que ja foi adicionado um novo contrutor de tipo para valor booleano
 
 s :: Valor -> String             
@@ -27,43 +27,45 @@ s (ValorStr str) = str
 i :: Valor -> Integer
 i (ValorInt vint) = vint 
 -- a funcao "b" abaixo recupera o valor booleano dentro de um valor
+b :: Valor -> Bool
 b (ValorBool vbool) = vbool
 
 
 instance Show Valor where
- show (ValorInt vint) = show vint
- show (ValorStr vstr) = vstr
- show (ValorBool vb) = show vb
+   show (ValorInt vint) = show vint
+   show (ValorStr vstr) = vstr
+   show (ValorBool vb) = show vb
 
 -- precisamos que Valor esteja em Eq para podermos especificar os casos de teste em Testes.hs
 instance Eq Valor where
- (ValorInt i1) == (ValorInt i2) =  i1 == i2
- (ValorStr s1) == (ValorStr s2) =  s1 == s2
- (ValorBool b1) == (ValorBool b2) = b1 == b2
+   (ValorInt i1) == (ValorInt i2) =  i1 == i2
+   (ValorStr s1) == (ValorStr s2) =  s1 == s2
+   (ValorBool b1) == (ValorBool b2) = b1 == b2
 
 
 -- note que a funcao eval retorna um Valor             
 eval :: RContext -> Exp -> Valor
 eval context x = case x of 
-    EAdd exp0 exp -> ValorInt ( i(eval context exp0) + i (eval context exp))
-    ESub exp0 exp -> ValorInt ( i(eval context exp0) - i (eval context exp))
-    EMul exp0 exp -> ValorInt ( i(eval context exp0) * i (eval context exp))
-    EDiv exp0 exp -> ValorInt ( i(eval context exp0) `div` i (eval context exp))
-    ECon exp0 exp -> ValorStr ( s(eval context exp0) ++ s(eval context exp))
-    EInt n  -> ValorInt n
-    EVar id  -> lookup context (getStr id)
-    EStr str -> ValorStr str
-    -- adicione abaixo um padrao e comportamento associado a expressao Or
+      EAdd exp0 exp -> ValorInt ( i(eval context exp0) + i (eval context exp))
+      ESub exp0 exp -> ValorInt ( i(eval context exp0) - i (eval context exp))
+      EMul exp0 exp -> ValorInt ( i(eval context exp0) * i (eval context exp))
+      EDiv exp0 exp -> ValorInt ( i(eval context exp0) `div` i (eval context exp))
+      ECon exp0 exp -> ValorStr ( s(eval context exp0) ++ s(eval context exp))
+      EInt n  -> ValorInt n
+      EVar id  -> lookup context (getStr id)
+      EStr str -> ValorStr str
+      -- adicione abaixo um padrao e comportamento associado a expressao Or
+      EOr exp0 exp -> ValorBool ( b(eval context exp0) || b(eval context exp))
+      -- adicione abaixo um padrao e comportamento associado a expressao And
+      EAnd exp0 exp -> ValorBool ( b(eval context exp0) && b(eval context exp))
+      -- adicione abaixo um padrao e comportamento associado a expressao Not
+      ENot exp -> ValorBool ( not (b(eval context exp)))
+      -- adicione abaixo um padrao e comportamento associado ao literal true
+      ETrue -> ValorBool True
+      -- adicione abaixo um padrao e comportamento associado ao literal false
+      EFalse -> ValorBool False 
 
-    -- adicione abaixo um padrao e comportamento associado a expressao And
-
-    -- adicione abaixo um padrao e comportamento associado a expressao Not
-
-    -- adicione abaixo um padrao e comportamento associado ao literal true
-
-    -- adicione abaixo um padrao e comportamento associado ao literal false
-
-    
+      
 type RContext = [(String,Valor)]
 
 getStr :: Ident -> String
